cc5c43174976548a4b20f0a83488033da037d1a6
magendror <magendror@gmail.com>
2020-11-25 21:00:59 +0200

    first upload priority_queue.c

--- priority_queue.c
+++ priority_queue.c
@@ -1,1 +1,21 @@
-asd
+#include "priority_queue.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+struct PriorityQueue_t{
+    struct PriorityQueue_t next;
+    PQElementPriority priority;
+    PQElement element;
+};
+
+PriorityQueue pqCreate(CopyPQElement copy_element,
+                       FreePQElement free_element,
+                       EqualPQElements equal_elements,
+                       CopyPQElementPriority copy_priority,
+                       FreePQElementPriority free_priority,
+                       ComparePQElementPriorities compare_priorities){
+    PriorityQueue queue=malloc(sizeof(PriorityQueue));
+    queue->element=NULL;
+    queue->priority=NULL;
+}
