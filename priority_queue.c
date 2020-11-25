a80f6e9b12bedfcde7020b9f4065934db968e06b
magendror <magendror@gmail.com>
2020-11-25 23:43:02 +0200

    PQCreate

--- priority_queue.c
+++ priority_queue.c
@@ -4,9 +4,15 @@
 #include <stdio.h>
 
 struct PriorityQueue_t{
-    struct PriorityQueue_t next;
+    struct PriorityQueue_t* next;
     PQElementPriority priority;
     PQElement element;
+    CopyPQElement copy_element;
+    FreePQElement free_element;
+    EqualPQElements equal_elements;
+    CopyPQElementPriority copy_priority;
+    FreePQElementPriority free_priority;
+    ComparePQElementPriorities compare_priorities;
 };
 
 PriorityQueue pqCreate(CopyPQElement copy_element,
@@ -18,4 +24,11 @@ PriorityQueue pqCreate(CopyPQElement copy_element,
     PriorityQueue queue=malloc(sizeof(PriorityQueue));
     queue->element=NULL;
     queue->priority=NULL;
+    queue->compare_priorities=compare_priorities;
+    queue->copy_priority=copy_priority;
+    queue->free_priority=free_priority;
+    queue->equal_elements=equal_elements;
+    queue->free_element=free_element;
+    queue->copy_element=copy_element;
+    return queue;
 }
