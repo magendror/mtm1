f64e086ec46e1522e96e52ddf310ec5249112685
magendror <magendror@gmail.com>
2020-12-01 10:21:47 +0200

    date struct

--- date.c
+++ date.c
@@ -1,9 +1,5 @@
-typedef enum Months_t {
-    MONTHS,JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC,MONTHS_NUM
-} Months;
-
 struct Date{
     int date;
-    Months month;
+    int month;
     int year;
 };
